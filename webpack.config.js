0fa0de7ccd8a988d215bb5b13f19d6c0100d4b52
VladimirRanski <ranski.vladimir@gmail.com>
2023-03-14 01:30:11 +0300

    fix: .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -15,75 +15,56 @@ isProd = !isDev;
 const filename = (ext) => (isDev ? `[name]${ext}` : `[name]_[contenthash]${ext}`);
 
 const optimization = () => {
-    const config = {
-        minimize: isProd,
-        splitChunks: {
-            chunks: 'all',
-        },
-    };
+	const config = {
+		minimize: isProd, splitChunks: {
+			chunks: 'all',
+		},
+	};
 
-    if (isProd) {
-        config.minimizer = [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.imageminMinify,
-                    options: {
-                        plugins: [
-                            ['gifsicle', {interlaced: true}],
-                            ['jpegtran', {progressive: true}],
-                            ['optipng', {optimizationLevel: 5}],
-                            [
-                                'svgo',
-                                {
-                                    plugins: [
-                                        {
-                                            name: 'preset-default',
-                                            params: {
-                                                overrides: {
-                                                    removeViewBox: false,
-                                                    addAttributesToSVGElement: {
-                                                        params: {
-                                                            attributes: [
-                                                                {xmlns: 'http://www.w3.org/2000/svg'},
-                                                            ],
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        },
-                                    ],
-                                },
-                            ],
-                        ],
-                    },
-                },
-            }),
-        ];
-    }
-    return config;
+	if (isProd) {
+		config.minimizer = [new CssMinimizerPlugin(), new TerserPlugin(), new ImageMinimizerPlugin({
+			minimizer: {
+				implementation: ImageMinimizerPlugin.imageminMinify, options: {
+					plugins: [['gifsicle', {interlaced: true}], ['jpegtran', {progressive: true}], ['optipng', {optimizationLevel: 5}], ['svgo', {
+						plugins: [{
+							name: 'preset-default', params: {
+								overrides: {
+									removeViewBox: false,
+									addAttributesToSVGElement: {
+										params: {
+											attributes: [{xmlns: 'http://www.w3.org/2000/svg'},],
+										},
+									},
+								},
+							},
+						},],
+					},],],
+				},
+			},
+		}),];
+	}
+	return config;
 };
 
 const cssLoader = (extra) => {
-    const loaders = [MiniCssExtractPlugin.loader, 'css-loader'];
-    if (extra) {
-        loaders.push(extra);
-    }
-    return loaders;
+	const loaders = [MiniCssExtractPlugin.loader, 'css-loader'];
+	if (extra) {
+		loaders.push(extra);
+	}
+	return loaders;
 };
 
 module.exports = {
-    context: path.resolve(__dirname, './vc-brest_site'),
-    entry: ['@babel/polyfill', './index.jsx'],
-    output: {
-        clean: true,
-        filename: `js/${filename('.js')}`,
-        path: path.resolve(__dirname, 'dist'),
-    },
-    experiments: {
-        topLevelAwait: true
-    },
+	context: path.resolve(__dirname, './vc-brest_site'),
+	entry: ['@babel/polyfill', './index.jsx'],
+	output: {
+		clean: true,
+		filename: `js/${filename('.js')}`,
+		path: path.resolve(__dirname, 'dist'),
+	},
+	experiments: {
+		topLevelAwait: true
+	},
 
 	resolve: {
 		extensions: ['.js', '.jsx', '.ts', '.css', '.scss', '.json'],
@@ -91,139 +72,92 @@ module.exports = {
 
 	devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
 
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'vc-brest_site'),
-        },
-        historyApiFallback: true,
-        liveReload: true,
-        compress: true,
-        port: 9000,
-        hot: true,
-    },
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'vc-brest_site'),
+		},
+		historyApiFallback: true,
+		liveReload: true,
+		compress: true,
+		port: 9000,
+		hot: true,
+	},
 
-    performance: {
-        hints: false,
-        maxAssetSize: 512000,
-        maxEntrypointSize: 512000,
-    },
+	performance: {
+		hints: false, maxAssetSize: 512000, maxEntrypointSize: 512000,
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './vc-brest_site/index.html'),
-            inject: 'body',
-            minify: {
-                collapseWhitespace: isProd,
-            },
-        }),
+	plugins: [new HtmlWebpackPlugin({
+		template: path.resolve(__dirname, './vc-brest_site/index.html'),
+		inject: 'body',
+		minify: {
+			collapseWhitespace: isProd,
+		},
+	}),
 
-        new MiniCssExtractPlugin({
-            filename: `./css/${filename('.css')}`,
-        }),
+		new MiniCssExtractPlugin({
+			filename: `./css/${filename('.css')}`,
+		}),
 
-        new ESLintPlugin(),
-        // new CopyWebpackPlugin(),
-    ],
+		new ESLintPlugin(), // new CopyWebpackPlugin(),
+	],
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: cssLoader(),
-            },
-            {
-                test: /\.css$/i,
-                use: cssLoader('postcss-loader'),
-            },
-            {
-                test: /\.less$/i,
-                use: cssLoader('less-loader'),
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: cssLoader('sass-loader'),
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: './front-end/assets/images/[name][ext]',
-                },
-            },
-            {
-                test: /\.(ico)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: './front-end/assets/icons/[name][ext]',
-                },
-            },
-            {
-                test: /\.svg$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: './front-end/assets/svg/[name][ext]',
-                },
+	module: {
+		rules: [{
+			test: /\.css$/i, use: cssLoader(),
+		}, {
+			test: /\.css$/i, use: cssLoader('postcss-loader'),
+		}, {
+			test: /\.less$/i, use: cssLoader('less-loader'),
+		}, {
+			test: /\.s[ac]ss$/i, use: cssLoader('sass-loader'),
+		}, {
+			test: /\.(png|jpg|jpeg|gif)$/i, type: 'asset/resource', generator: {
+				filename: './front-end/assets/images/[name][ext]',
+			},
+		}, {
+			test: /\.(ico)$/i, type: 'asset/resource', generator: {
+				filename: './front-end/assets/icons/[name][ext]',
+			},
+		}, {
+			test: /\.svg$/i, type: 'asset/resource', generator: {
+				filename: './front-end/assets/svg/[name][ext]',
+			},
 
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: './front-end/assets/fonts/[name][ext]',
-                },
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-                generator: {
-                    filename: './front-end/assets/csv/' + `${filename}`,
-                },
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-                generator: {
-                    filename: './front-end/assets/xml/' + `${filename}`,
-                },
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                },
-            },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-typescript'
-                        ]
-                    }
-                },
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ]
-                    }
-                },
-            }
-        ],
-    },
+		}, {
+			test: /\.(woff2?|eot|ttf|otf)$/i,
+			type: 'asset/resource',
+			generator: {
+				filename: './front-end/assets/fonts/[name][ext]',
+			},
+		}, {
+			test: /\.(csv|tsv)$/i, use: ['csv-loader'], generator: {
+				filename: './front-end/assets/csv/' + `${filename}`,
+			},
+		}, {
+			test: /\.xml$/i, use: ['xml-loader'], generator: {
+				filename: './front-end/assets/xml/' + `${filename}`,
+			},
+		}, {
+			test: /\.js$/, exclude: /node_modules/, use: {
+				loader: "babel-loader", options: {
+					presets: ['@babel/preset-env']
+				}
+			},
+		}, {
+			test: /\.ts$/, exclude: /node_modules/, use: {
+				loader: "babel-loader", options: {
+					presets: ['@babel/preset-env', '@babel/preset-typescript']
+				}
+			},
+		}, {
+			test: /\.jsx$/, exclude: /node_modules/, use: {
+				loader: "babel-loader", options: {
+					presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			},
+		}],
+	},
 
-    optimization: optimization(),
+	optimization: optimization(),
 };
