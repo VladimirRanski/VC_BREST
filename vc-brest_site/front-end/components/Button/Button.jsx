428be24cc9c94ea541527eaedb059ad25b2cb2d5
VladimirRanski <ranski.vladimir@gmail.com>
2023-03-14 22:38:18 +0300

    feat: add functional on button component

--- vc-brest_site/front-end/components/Button/Button.jsx
+++ vc-brest_site/front-end/components/Button/Button.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import './styles/index.scss';
 
+const classes = ['btn'];
+
+const AddingClasses = (props) => {
+	for (let key in props) {
+		if (((key !== 'href') && (key !== 'text')) && props[key] !== undefined) {
+			classes.push(props[key])
+		}
+	}
+}
 
 class Button extends React.Component {
 
 	render() {
+		AddingClasses(this.props);
 		return (
-				<a className='btn' href='#'>{this.props.text}</a>
+				<a className={`${classes.join(' ')}`} href={this.props.href}>{this.props.text}</a>
 		);
 	}
 }
