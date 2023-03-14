ae9804918cde0f6ad58c9c8e78e24a8fb77ff46f
VladimirRanski <ranski.vladimir@gmail.com>
2023-03-14 18:00:04 +0300

    feat: add header

---
+++ vc-brest_site/front-end/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import './styles/index.scss';
+
+
+class Header extends React.Component {
+
+	render() {
+		return (
+			<header className="header">
+				<div className="container">
+					<nav className="nav">
+						<a className="nav__logo" href="#">
+							<img className="nav__logo--img"
+									 src="./front-end/assets/svg/logo_vc_brest.svg" width="83"
+									 height="56" alt="логотип волейбольного клуба Брест"/>
+						</a>
+						<ul className="nav__list">
+							<li className="nav__item"><a className="nav__link" href="#">Расписание
+								игр</a></li>
+							<li className="nav__item" onClick={this.toggleHidden}><a
+								className="nav__link nav__link--selections" href="#">Команды</a>
+								<ul className="nav__selections hidden">
+									<li className="nav__item"><a className="nav__link" href="#">«Западный
+										Буг»</a></li>
+									<li className="nav__item"><a className="nav__link"
+																							 href="#">«Прибужье»</a></li>
+								</ul>
+							</li>
+							<li className="nav__item"><a className="nav__link"
+																					 href="#">Новости</a></li>
+							<li className="nav__item"><a className="nav__link"
+																					 href="#">Галерея</a></li>
+							<li className="nav__item"><a className="nav__link" href="#">О
+								клубе</a></li>
+							<li className="nav__item"><a className="nav__link"
+																					 href="#">Контакты</a></li>
+							<li className="nav__item"><a className="nav__link"
+																					 href="#">Обращения</a></li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+		);
+	}
+
+	toggleHidden(e) {
+		if (e.target.nextElementSibling !== null) {
+			if (e.target.nextElementSibling.classList.contains('nav__selections')) {
+				e.target.nextElementSibling.classList.toggle('hidden')
+				if (!e.target.nextElementSibling.classList.contains('hidden')) {
+					e.target.nextElementSibling.style.display = 'flex'
+				} else {
+					e.target.nextElementSibling.style.display = 'none'
+				}
+			}
+		}
+	}
+
+}
+
+export default Header
--- vc-brest_site/index.jsx
+++ vc-brest_site/index.jsx
@@ -1,6 +1,13 @@
-import './front-end/index.scss'
-import {test} from "./front-end/scripts/test";
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-test(4,9)
+import './front-end/styles/index.scss';
 
+import Header from "./front-end/components/Header/Header";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<Header/>
+	</React.StrictMode>
+);
