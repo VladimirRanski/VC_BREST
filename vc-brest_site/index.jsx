5cf9fdf1e7d8ca8d6f6440a3883fc6a82c315a51
VladimirRanski <ranski.vladimir@gmail.com>
2023-03-16 00:16:06 +0300

    feat: add games calendar component

--- vc-brest_site/index.jsx
+++ vc-brest_site/index.jsx
@@ -5,11 +5,14 @@ import './front-end/styles/index.scss';
 
 import Header from "./front-end/components/Header/Header";
 import Footer from "./front-end/components/Footer/Footer";
+import Games_calendar
+	from "./front-end/components/Games_calendar/Games_calendar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Header/>
+		<Games_calendar/>
 		<Footer/>
 	</React.StrictMode>
 );
